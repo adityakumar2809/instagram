cfcd2057007e33d051798257e5d83faaba8e1e8b
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-12-24 18:27:42 +0530

    Configure ImagePicker to choose images from gallery

--- src/components/main/Add.js
+++ src/components/main/Add.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [camera, setCamera] = useState(null);
     const [image, setImage] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+
         })();
     }, []);
 
@@ -22,10 +28,23 @@ export default function App() {
         }
     }
 
-    if (hasPermission === null) {
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    if (hasCameraPermission === null || hasGalleryPermission === null) {
         return <View />;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
         return <Text>No access to camera</Text>;
     }
     return (
@@ -55,13 +74,19 @@ export default function App() {
                         takePicture()
                     )}
                 />
-                { 
+                <Button
+                    title="Pick Image From Gallery"
+                    onPress={() => (
+                        pickImage()
+                    )}
+                />
+                {
                     image
-                    ?   <Image 
-                            source={{ uri: image }} 
-                            style={styles.imageStyle} 
+                        ? <Image
+                            source={{ uri: image }}
+                            style={styles.imageStyle}
                         />
-                    :   <></>
+                        : <></>
                 }
             </View>
         </View>
