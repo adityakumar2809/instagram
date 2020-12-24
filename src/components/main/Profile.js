b32e7da5f713f848d896388e2c0b6f7e95f4a7a5
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-12-24 19:50:35 +0530

    Render posts as Flatlist on profile page

--- src/components/main/Profile.js
+++ src/components/main/Profile.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { View, Text, Image, FlatList } from 'react-native'
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
 
 import { connect } from 'react-redux'
 
 function Profile(props) {
     const { currentUser, posts } = props;
-    console.log({currentUser, posts});
     return (
-        <View>
-            <Text>Profile</Text>
+        <View style={styles.containerStyle}>
+            <View style={styles.infoContainerStyle}>
+                <Text>{currentUser.name}</Text>
+                <Text>{currentUser.email}</Text>
+            </View>
+            <View style={styles.galleryContainerStyle}>
+                <FlatList
+                    numColumns={3}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={({item}) => (
+                        <View style={styles.imageContainerStyle}>
+                            <Image
+                                source={{uri: item.downloadURL}}
+                                style={styles.imageStyle}
+                            />
+                        </View>
+                    )} 
+                />
+            </View>
         </View>
     )
 }
@@ -16,6 +33,26 @@ function Profile(props) {
 const mapStateToProps = (store) => ({
     currentUser: store.userState.currentUser,
     posts: store.userState.posts,
+})
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flex: 1,
+        marginTop: 40
+    },
+    infoContainerStyle: {
+        margin: 20
+    },
+    galleryContainerStyle: {
+
+    },
+    imageContainerStyle: {
+        flex: 1/3
+    },
+    imageStyle: {
+        flex: 1,
+        aspectRatio: 1/1
+    }
 })
 
 export default connect(mapStateToProps, null)(Profile)
